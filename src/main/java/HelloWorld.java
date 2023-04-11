4d94350de04b62c026544c93f39b387603aaab19
Rick Spencer <rick@influxdata.com>
2023-04-11 17:26:26 -0400

    first commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,18 @@
+package com.example.helloworld;
+
+// Import Apache Arrow Flight SQL classes
+import org.apache.arrow.flight.FlightDescriptor;
+import org.apache.arrow.flight.FlightInfo;
+import org.apache.arrow.flight.FlightStream;
+import org.apache.arrow.flight.Location;
+import org.apache.arrow.flight.Ticket;
+import org.apache.arrow.flight.sql.FlightSqlClient;
+import org.apache.arrow.vector.VectorSchemaRoot;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        // Use Apache Arrow Flight SQL classes here
+    }
+}
