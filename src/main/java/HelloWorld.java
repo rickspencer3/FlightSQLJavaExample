7abb5621d39115b6c89695b05334fa38cde561e3
Rick Spencer <rick@influxdata.com>
2023-04-12 16:27:14 -0400

    added boiler plate to create a FlightSqlClient instance

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,18 +1,20 @@
 package com.example.helloworld;
 
 // Import Apache Arrow Flight SQL classes
-import org.apache.arrow.flight.FlightDescriptor;
-import org.apache.arrow.flight.FlightInfo;
-import org.apache.arrow.flight.FlightStream;
-import org.apache.arrow.flight.Location;
-import org.apache.arrow.flight.Ticket;
+import org.apache.arrow.flight.*;
 import org.apache.arrow.flight.sql.FlightSqlClient;
+import org.apache.arrow.memory.BufferAllocator;
+import org.apache.arrow.memory.RootAllocator;
 import org.apache.arrow.vector.VectorSchemaRoot;
 
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-
-        // Use Apache Arrow Flight SQL classes here
+        String host = "us-east-1-1.aws.cloud2.influxdata.com";
+        // // Use Apache Arrow Flight SQL classes here
+        Location location = Location.forGrpcInsecure(host, 50051);
+        BufferAllocator allocator = new RootAllocator(Long.MAX_VALUE);
+        FlightClient flightClient = FlightClient.builder(allocator, location).build();
+        FlightSqlClient sqlClient = new FlightSqlClient(flightClient);
     }
 }
